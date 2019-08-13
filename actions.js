7615b880b3fe95feb2fc9a4f131b00dbde93de1b
Łukasz Nolka <nolkalukasz@gmail.com>
2019-08-13 14:16:56 +0200

    Update votes actions

--- actions.js
+++ actions.js
@@ -9,13 +9,13 @@ const UNLIKE_COMMENT = 'UNLIKE_COMMENT';
 const createComment = (text, id) => ({type: CREATE_COMMENT, text, id});
 const editComment = (text, id) => ({type: EDIT_COMMENT, text, id});  
 const deleteComment = (id) => ({type: DELETE_COMMENT, id});
-const likeComment = (likes, id) => ({type: LIKE_COMMENT, likes, id});
-const unlikeComment = (likes, id) => ({type: UNLIKE_COMMENT, likes, id});
+const likeComment = (id) => ({type: LIKE_COMMENT, id});
+const unlikeComment = (id) => ({type: UNLIKE_COMMENT, id});
 
 
 //Examples
 dispatch(addComment('nowy komentarz!', uuid.v4()));
 dispatch(editComment('nowa treść', 'asdhasdkjasda21312dsa'));
 dispatch(deleteComment('asdhasdkjasda21312dsa'));
-dispatch(likeComment(233, 'asdhasdkjasda21312dsa'));
-dispatch(unlikeComment(234, 'asdhasdkjasda21312dsa'));
+dispatch(likeComment('asdhasdkjasda21312dsa'));
+dispatch(unlikeComment('asdhasdkjasda21312dsa'));
